8e5ad4ecaaff20077de911ed10d1d1c08cad6652
Shin Ho Geun <shg9611@naver.com>
2025-06-23 17:35:53 +0900

    default page

---
+++ src/main/java/com/firstproject/crud/ViewController.java
@@ -0,0 +1,16 @@
+package com.firstproject.crud;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class ViewController {
+
+    @GetMapping("/")
+    public String defaultPage(Model model){
+
+        model.addAttribute("username","신호근");
+        return "defaultPage";
+    }
+}
