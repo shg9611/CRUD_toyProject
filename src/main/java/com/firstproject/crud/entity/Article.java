7d9f10370e964857e7e6f32797bdcc8b2cb3db78
Shin Ho Geun <shg9611@naver.com>
2025-06-27 18:18:29 +0900

    update application.properties for sql

--- src/main/java/com/firstproject/crud/entity/Article.java
+++ src/main/java/com/firstproject/crud/entity/Article.java
@@ -1,9 +1,6 @@
 package com.firstproject.crud.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -18,7 +15,7 @@ public class Article {
 
 
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column
